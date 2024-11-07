9461c9deb2b362c2db78c81eefe8ba237e9bd6f1
FortySixAnd2 <jmaxj91j@gmail.com>
2024-11-08 00:57:29 +0300

    Add function checkMeetingTime.

---
+++ js/functions.js
@@ -0,0 +1,31 @@
+/**
+ * Конвертирует время из строки в минуты.
+ * @param {string} timeString Строка со временем вида '08:30'.
+ * @returns {number} Время в минутах.
+ */
+const convertTimeStringToMinutes = (timeString) => {
+  const timeParts = timeString.split(':');
+  const timeMinutes = (timeParts[0] * 60) + +timeParts[1];
+  return timeMinutes;
+};
+
+
+/**
+ * Проверка: выходит ли встреча за рамки рабочего дня.
+ * @param {string} dayStart Время начала рабочего дня.
+ * @param {string} dayEnd Время конца рабочего дня.
+ * @param {string} meetingStart Время старта встречи.
+ * @param {number} meetingDuration Продолжительность встречи.
+ * @returns {boolean} Встреча не выходит за рамки рабочего дня.
+ */
+const checkMeetingTime = (dayStart = '', dayEnd = '', meetingStart = '', meetingDuration = 0) => {
+  const dayStartMunites = convertTimeStringToMinutes(dayStart);
+  const dayEndMunites = convertTimeStringToMinutes(dayEnd);
+  const meetingStartMunites = convertTimeStringToMinutes(meetingStart);
+
+  if (meetingStartMunites >= dayStartMunites && meetingStartMunites <= (dayEndMunites - meetingDuration)) {
+    return true;
+  }
+  return false;
+};
+checkMeetingTime();
