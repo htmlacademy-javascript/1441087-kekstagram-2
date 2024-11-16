ed99290e8672c29dd1a53f9a7f7518e7ce5827c0
Keks <keks@htmlacademy.ru>
2024-11-16 21:22:32 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/comments.js
@@ -0,0 +1,58 @@
+import { getComment } from './get-comment.js';
+
+
+const bigPicture = document.querySelector('.big-picture');
+const socialComments = bigPicture.querySelector('.social__comments');
+const socialCommentTotalCount = bigPicture.querySelector('.social__comment-total-count');
+const socialCommentShownCount = bigPicture.querySelector('.social__comment-shown-count');
+const commentsLoader = bigPicture.querySelector('.comments-loader');
+
+
+const COMMENTS_SHOW_STEP = 5;
+let commentsLimitCurrent = COMMENTS_SHOW_STEP;
+let comments = [];
+
+
+const showMoreComments = () => {
+  const commentsToDisplay = comments.slice(commentsLimitCurrent - COMMENTS_SHOW_STEP, commentsLimitCurrent);
+
+  commentsToDisplay.forEach((commentData) => {
+    socialComments.append(getComment(commentData));
+  });
+
+  commentsLimitCurrent += COMMENTS_SHOW_STEP;
+
+  const CommentShownCount = socialComments.children.length;
+  socialCommentShownCount.textContent = CommentShownCount;
+
+  if (CommentShownCount === comments.length) {
+    commentsLoader.classList.add('hidden');
+  }
+};
+
+
+/**
+ * Вставляет на страницу комментарии для переданной фотографии.
+ * @param {object} photoData Данные фотографии.
+ */
+const insertComments = (photoData) => {
+  comments = photoData.comments;
+
+  socialCommentTotalCount.textContent = photoData.comments.length;
+  showMoreComments();
+};
+
+
+const cleanComments = () => {
+  commentsLimitCurrent = COMMENTS_SHOW_STEP;
+  comments = [];
+  while (socialComments.firstChild) {
+    socialComments.removeChild(socialComments.firstChild);
+  }
+  socialCommentTotalCount.textContent = '';
+  socialCommentShownCount.textContent = '';
+  commentsLoader.classList.remove('hidden');
+};
+
+
+export { insertComments, showMoreComments, cleanComments };
