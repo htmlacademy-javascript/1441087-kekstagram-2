389507f2c9db507bbea43300b58e4f7e62e308da
FortySixAnd2 <jmaxj91j@gmail.com>
2024-12-15 14:12:38 +0300

    refactor: remove all mock-data modules and functions.

--- js/util.js
+++ js/util.js
@@ -1,71 +1,4 @@
 /**
- * Возвращает случайное целое число из заданного диапазона.
- * @param {number} min - минимальное значение.
- * @param {number} max - максимальное значение.
- * @returns {number} случайное число из диапазона.
- */
-const getRandomInteger = (min, max) => {
-  const lower = Math.ceil(Math.min(Math.abs(min), Math.abs(max)));
-  const upper = Math.floor(Math.max(Math.abs(min), Math.abs(max)));
-  const result = Math.random() * (upper - lower + 1) + lower;
-
-  return Math.floor(result);
-};
-
-
-/**
- * Возвращает случайный элемент из списка.
- * @param {array} elements - список элементов.
- * @returns случайный элемент из списка.
- */
-const getRandomArreyElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
-
-
-/**
- * Создаёт генератор ID по порядку от 1 до заданного значения.
- * Когда значения в диапазоне закончатся — будет возвращаться null.
- * @param {number} max - максимальное значение.
- * @returns {number} очередной ID по порядку.
- */
-const getIdGenerator = (max) => {
-  let currentValue = 0;
-
-  return function () {
-    if (currentValue >= max) {
-      return null;
-    }
-    currentValue += 1;
-    return currentValue;
-  };
-};
-
-
-/**
- * Создаёт генератор случайных ID. Генератор принимает диапазон значений,
- * из которого он будет выбирать значения случайным образом. Когда значения в диапазоне
- * закончатся — будет возвращаться null.
- * @param {number} min - минимальное значение.
- * @param {number} max - максимальное значение.
- * @returns {number} уникальный ID из диапазона.
- */
-const getRandomIdFromRangeGenerator = (min, max) => {
-  const previousValues = [];
-
-  return function () {
-    let currentValue = getRandomInteger(min, max);
-    if (previousValues.length >= (max - min + 1)) {
-      return null;
-    }
-    while (previousValues.includes(currentValue)) {
-      currentValue = getRandomInteger(min, max);
-    }
-    previousValues.push(currentValue);
-    return currentValue;
-  };
-};
-
-
-/**
  * Перемешивает элементы массива в случайном порядке.
  * @param {array} array Массив.
  * @returns
@@ -77,7 +10,7 @@ const shuffleArray = (array) => {
 
 
 /**
- * Является ли нажатая в событии клавиша клавишей Escape.
+ * Проверяет является ли нажатая в событии клавиша клавишей Escape.
  * @param {object} evt Событие.
  * @returns
  */
@@ -101,10 +34,6 @@ const debounce = (callback, timeoutDelay = 500) => {
 
 
 export {
-  getRandomInteger,
-  getRandomArreyElement,
-  getRandomIdFromRangeGenerator,
-  getIdGenerator,
   shuffleArray,
   isEscapeKey,
   debounce
